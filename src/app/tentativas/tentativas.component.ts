641492597b60fcb650c5f16cc88040991ea485d9
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-18 14:53:47 -0300

    Property Biding.

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tentativas.component.css']
 })
 export class TentativasComponent implements OnInit {
-
+  public coracaoVazio: string = '/assets/coracao_vazio.png'
+  public coracaoCheio: string = '/assets/coracao_cheio.png'
   constructor() { }
 
   ngOnInit(): void {
