4d8d677c2b257947d0e53ec0dc2e568a50ed72c8
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-22 11:20:32 -0300

    Setting the logic of phrase rounds.

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -14,13 +14,23 @@ export class PainelComponent implements OnInit {
   public instrucao: string = "Traduza a frase:"
   public resposta: string
 
-  constructor() { console.log(this.frases) }
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada] 
+    console.log(this.rodadaFrase) 
+  }
 
   ngOnInit(): void {
   }
 
-  public atualizaResposta(resposta: Event): void{
+  public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value
-    console.log(this.resposta)
+    //console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    console.log('Verificar resposta: ', this.resposta)
   }
 }
