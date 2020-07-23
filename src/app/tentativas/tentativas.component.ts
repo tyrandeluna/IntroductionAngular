2131acdabee4ab0edd8f00ac421064eadb3cdc95
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-22 21:07:53 -0300

    Class "Coracao".

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Coracao } from '../shared/coracao.model'
 
 @Component({
   selector: 'app-tentativas',
@@ -8,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class TentativasComponent implements OnInit {
   public coracaoVazio: string = '/assets/coracao_vazio.png'
   public coracaoCheio: string = '/assets/coracao_cheio.png'
+
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
