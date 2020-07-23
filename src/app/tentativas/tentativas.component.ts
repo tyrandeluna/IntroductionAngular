e57517940186e88561363f35262c78f515d8af29
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-22 21:26:06 -0300

    Property biding "painel" and "tentativas".

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Coracao } from '../shared/coracao.model'
 
 @Component({
@@ -9,6 +9,8 @@ import { Coracao } from '../shared/coracao.model'
 export class TentativasComponent implements OnInit {
   public coracaoVazio: string = '/assets/coracao_vazio.png'
   public coracaoCheio: string = '/assets/coracao_cheio.png'
+
+  @Input() public tentativas: number
 
   public coracoes: Coracao[] = [
     new Coracao(true),
