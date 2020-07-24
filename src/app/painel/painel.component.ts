0f7c60ff6a4e900f6fccb6e6d5ac9d44052359a6
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-24 09:38:59 -0300

    Victory and defeat.

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -36,13 +36,17 @@ export class PainelComponent implements OnInit {
   public verificarResposta(): void {
 
     if(this.rodadaFrase.frasePtBr == this.resposta) {
-      alert('A tradução está correta!')
 
       //trocar pergunta da rodada
       this.rodada++
 
       //progresso
       this.progresso = this.progresso + (100 / this.frases.length)
+
+      //
+      if(this.rodada === 4) {
+        alert('Concluiu as traduções com sucesso!')
+      }
 
       //atualiza o objeto rodadaFrase
       this.atualizaRodada()
