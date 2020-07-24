78959e6cb43db585318599d7fcfb8cbec99c9d85
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-24 09:24:45 -0300

    ngOnChanges

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { Coracao } from '../shared/coracao.model'
 
 @Component({
@@ -6,7 +6,7 @@ import { Coracao } from '../shared/coracao.model'
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
   public coracaoVazio: string = '/assets/coracao_vazio.png'
   public coracaoCheio: string = '/assets/coracao_cheio.png'
 
@@ -20,7 +20,12 @@ export class TentativasComponent implements OnInit {
 
   constructor() { }
 
+  ngOnChanges() {
+    console.log('tentativas recebidas do painel: ', this.tentativas)
+  }
+
   ngOnInit(): void {
+    
   }
 
 }
